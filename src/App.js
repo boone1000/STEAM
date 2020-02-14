5c8fb030f07783ef98aba5dadde22d2357d435bb
Evan Feenstra <evan@evanfeenstra.com>
2020-02-14 10:06:13 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 
 function App() {
-  return (
+  return <BrowserRouter>
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/contact">Contact</Link>
       </header>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contact" component={Contact} />
+
     </div>
-  );
+  </BrowserRouter>
 }
 
 export default App;

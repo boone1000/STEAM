ac3531ce7155e28de4e56145d51f39b693bc5bd5
Ashley Boone <ashley@Ashleys-MacBook-Pro.local>
2020-03-10 23:02:40 -0700

    docs folder

---
+++ docs/precache-manifest.437650931419376c221ffe935ce3cb66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94e69394652ff4ffd7247664fa1919f",
+    "url": "/STEAM/index.html"
+  },
+  {
+    "revision": "c2ef5618efb04fce52fc",
+    "url": "/STEAM/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "db6874333703fed41710",
+    "url": "/STEAM/static/css/main.15fb1c6e.chunk.css"
+  },
+  {
+    "revision": "c2ef5618efb04fce52fc",
+    "url": "/STEAM/static/js/2.43d4a8c7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/STEAM/static/js/2.43d4a8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6874333703fed41710",
+    "url": "/STEAM/static/js/main.c1f56172.chunk.js"
+  },
+  {
+    "revision": "bd10cfa88a9b42a50323",
+    "url": "/STEAM/static/js/runtime-main.85c8ed68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/STEAM/static/media/logo.5d5d9eef.svg"
+  }
+]);
